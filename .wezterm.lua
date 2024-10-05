f16d708b7f5ce562d20c9e867fc37ef231b9f608
n1xn <nkalakovic@gmail.com>
2024-10-06 00:43:54 +0200

    feat(wezterm): disable tab bar

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,8 +2,9 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.font_size = 16
+config.enable_tab_bar = false
 
+config.font_size = 16
 config.font = wezterm.font("JetBrains Mono")
 
 return config
