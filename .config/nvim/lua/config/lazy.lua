e00ec185e5c0886fcffce3d1d6d1ba9817aa042e
n1xn <nkalakovic@gmail.com>
2024-10-06 14:47:51 +0200

    feat(nvim): ruby support
    - working with .rubocop.yml

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -18,6 +18,10 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+
+    -- custom imports
+    { import = "lazyvim.plugins.extras.lang.ruby" },
+
     -- import/override with your plugins
     { import = "plugins" },
   },
