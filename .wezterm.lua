d03316308a372adfd1e099d094206110b4104af7
n1xn <nkalakovic@gmail.com>
2024-10-06 02:05:45 +0200

    feat(wezterm&aerospace): resizing wezterm
    - changed resize command for better convenience to enter
    - wezterm starting now with dynamic window size enables aerospace
    fullscreen toggles

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,6 +4,10 @@ local config = wezterm.config_builder()
 
 config.enable_tab_bar = false
 
+config.window_background_opacity = 0.93
+config.macos_window_background_blur = 30
+config.window_decorations = "RESIZE"
+
 config.font_size = 16
 config.font = wezterm.font("JetBrains Mono")
 
