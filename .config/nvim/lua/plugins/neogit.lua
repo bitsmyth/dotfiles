4dcff2054e739167d38560c4daafd8ac62d10be8
n1xn <nkalakovic@gmail.com>
2024-10-09 20:25:37 +0200

    fix(neogit): added base config
    configurations were missing

---
+++ .config/nvim/lua/plugins/neogit.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      -- required
+      "nvim-lua/plenary.nvim",
+      -- optional
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
+      "ibhagwan/fzf-lua",
+      "echasnovski/mini.pick",
+    },
+    config = true,
+  },
+}
