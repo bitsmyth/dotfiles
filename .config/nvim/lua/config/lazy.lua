3b5d3d36b7aa438c88566d14b8d573fb77e3ddde
n1xn <nkalakovic@gmail.com>
2024-10-15 06:37:11 +0200

    feat(nvim): added mini surround

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -27,6 +27,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
 
     -- import/override with your plugins
     { import = "plugins" },
