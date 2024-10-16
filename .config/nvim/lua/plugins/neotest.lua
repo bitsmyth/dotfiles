ba92c0232007854b59e8006e093a4dee95a9c03c
n1xn <nkalakovic@gmail.com>
2024-10-16 11:43:26 +0200

    fix(neotest): debugging neotests
    Switched neotest-minitest origin to enable debugging

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -2,12 +2,29 @@ return {
   "nvim-neotest/neotest",
   lazy = true,
   dependencies = {
-    "zidhuss/neotest-minitest",
+    {
+      "n1xn/neotest-minitest",
+      -- dir = "~/github/neotest-minitest",
+      -- dev = true,
+    },
   },
   config = function()
     require("neotest").setup({
+      -- log_level = vim.log.levels.DEBUG,
       adapters = {
         require("neotest-minitest"),
+      },
+      icons = {
+        expanded = "",
+        child_prefix = "",
+        child_indent = "",
+        final_child_prefix = "",
+        non_collapsible = "",
+        collapsed = "",
+        passed = "",
+        running = "",
+        failed = "",
+        unknown = "",
       },
     })
   end,
